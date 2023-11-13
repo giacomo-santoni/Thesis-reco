bda5caca477867de34f130eeb35b7a9e9d1caba7
giacomo-santoni <133137485+giacomo-santoni@users.noreply.github.com>
2023-11-13 17:40:29 +0100

    right scale plots and cuts

--- mask-reco-tools/lpc_analysis.py
+++ mask-reco-tools/lpc_analysis.py
@@ -11,6 +11,37 @@ from lpcclass import LPCSolver
 from recodisplay import load_pickle, parseArgs
 from geom_import import load_geometry
 
+def SetParameters():
+   epsilon = 36
+   min_points = 6
+   bandwidth = 30
+   stepsize = 30
+
+   cuts = [100,500]
+   g_cut_perc = 0.05
+   it = 500
+   g_it = 200
+   return epsilon, min_points, bandwidth, stepsize, it, g_it, cuts, g_cut_perc
+
+def EventNumberList(fname):
+   f = open(fname, "r")
+   events_string = []
+   all_events = []
+
+   for x in f:
+      events_string.append(x)
+   
+   for i in events_string:
+    new_ev = "".join(x for x in i if x.isdecimal())
+    all_events.append(int(new_ev))
+   return all_events
+
+def gradient(data):
+  gx_recodata, gy_recodata, gz_recodata = np.gradient(data)
+  gx_recodata, gy_recodata, gz_recodata = np.asarray(gx_recodata), np.asarray(gy_recodata), np.asarray(gz_recodata)
+  g_recodata = np.abs(np.sqrt((gx_recodata**2)+(gy_recodata**2)+(gz_recodata**2)))
+  return g_recodata
+
 def getVoxelsCut(data, geotree, cut):
   """returns voxel centers and amplitude of voxels whose amplitude is above a given threshold"""
   xform = geotree.gxform()[0:3,0:3]
@@ -33,7 +64,37 @@ def getVoxelsCut(data, geotree, cut):
   amps = data[(data>=cut).astype(bool)]
   return xyz, amps
 
+"""creo array con i centri e le ampiezze di tutti gli eventi selezionati"""
+def AllCentersAllEvents(events, fname1, fname2, applyGradient):
+  _, _, _, _, it, g_it, cuts, g_cut_perc = SetParameters()
+  centers_all_ev = []
+  amps_all_ev = []
+  recodata_all_ev = []
+  for ev in events:
+    data = load_pickle(fname1, ev)
+    if it not in data.keys():
+      data = load_pickle(fname2, ev)
+    if applyGradient:
+       recodata = data[g_it]
+       data_masked = recodata[7:-7,7:-7,7:-7]
+       gradient_recodata = gradient(data_masked)
+       g_cut = g_cut_perc*np.max(gradient_recodata)
+       centers, amps = getVoxelsCut(gradient_recodata, geom.fiducial, g_cut)
+    else:
+       recodata = data[it]
+       data_masked = recodata[5:-5,5:-5,5:-5]
+       cut = cuts[0]
+       centers, amps = getVoxelsCut(data_masked, geom.fiducial, cut)
+    centers = np.transpose(centers)
+    recodata_all_ev.append(recodata)
+    centers_all_ev.append(centers)
+    amps_all_ev.append(amps)
+  recodata_all_ev = np.asarray(recodata_all_ev)
+  return centers_all_ev, amps_all_ev, recodata_all_ev
+
+
 def Clustering(centers, amps):
+    epsilon, min_points, _, _, _, _, _, _ = SetParameters()
     # Calculate NN
     nearest_neighbors = NearestNeighbors(n_neighbors=2)
     neighbors = nearest_neighbors.fit(centers)
@@ -45,17 +106,16 @@ def Clustering(centers, amps):
     # sns.lineplot(x = i, y = distances)
     # plt.xlabel("Points")
     # plt.ylabel("Distance")
-    #plt.show()
     kneedle = kn.KneeLocator(x = range(1, len(distances)+1), y = distances, S = 1.0, 
                       curve = "concave", direction = "increasing", online=True)
     # get the estimate of knee point
-    epsilon = kneedle.knee_y
-    print(epsilon)
-    kneedle.plot_knee()
+    # epsilon = kneedle.knee_y
+    # print("eps: ", epsilon)
+    # kneedle.plot_knee()
     #plt.show()
 
-    dbscan_opt=DBSCAN(eps=epsilon, min_samples=6)
-    y_pred = dbscan_opt.fit_predict(centers)
+    db_algo = DBSCAN(eps = epsilon, min_samples = min_points)
+    y_pred = db_algo.fit_predict(centers)
     labels = np.unique(y_pred)
 
     all_clusters_in_ev = []
@@ -70,63 +130,50 @@ def Clustering(centers, amps):
        cluster = np.asarray(cluster)
        all_clusters_in_ev.append(cluster) 
        all_amps_in_ev.append(amp)
-    return all_clusters_in_ev, all_amps_in_ev, y_pred
-
-def PlotClusters(centers, amps):
-  # Plotting the resulting clusters
-    _,_,y_pred = Clustering(centers, amps)
-    fig1 = plt.figure()
-    ax = fig1.add_subplot(projection='3d')
-    ax.scatter3D(centers[:, 0], centers[:,1], centers[:,2], c = y_pred, cmap = 'cividis',s=15)
-    #ax.scatter3D(curve[:, 0], curve[:,1], curve[:,2], color = 'red')
-    plt.title("event: ", )
-    plt.legend()
-    plt.show()
+    return all_clusters_in_ev, all_amps_in_ev, y_pred, labels
 
 def LPC(centers, amps):
+    _,_,h,t,_,_,_,_ = SetParameters()
     """compute LPC"""
     lpc = LPCSolver()
     lpc.setPoints(centers, amps)
-    lpc.setBandwidth(30) #find right parameter
-    lpc.setStepSize(30) #find right parameter  
+    lpc.setBandwidth(h) #find right parameter
+    lpc.setStepSize(t) #find right parameter  
     lpc.solve()      
     #lpc.plot()
     curve = lpc.lpcPoints
     #plt.show()  
     return curve
 
-def ClustersLPC(clusters, amps):
+def ClustersLPC(clusters, amps, labels):
     all_curves = []
     for i in range(len(clusters)):
-      if i != 0:
+      #print(labels[i])
+      if labels[i] != -1:
         curve = LPC(clusters[i], amps[i])
         curve = np.asarray(curve)
         all_curves.append(curve)
     return all_curves
+
+def PlotClusters(events, fname1, fname2, applyGradient):
+  for i in range(len(events)):
+    centers_all_ev, amps_all_ev, recodata_all_ev = AllCentersAllEvents(events, fname1, fname2, applyGradient)
+    all_clusters_in_ev, all_amps_in_ev, y_pred, labels= Clustering(centers_all_ev[i], amps_all_ev[i])
+    all_curves_in_ev = ClustersLPC(all_clusters_in_ev,all_amps_in_ev,labels)
     
-def EventNumberList(fname):
-   f = open(fname, "r")
-   events_string = []
-   all_events = []
-
-   for x in f:
-      events_string.append(x)
-   
-   for i in events_string:
-    new_ev = "".join(x for x in i if x.isdecimal())
-    all_events.append(int(new_ev))
-   return all_events
-
-def gradient(data):
-  gx_recodata, gy_recodata, gz_recodata = np.gradient(data)
-  gx_recodata, gy_recodata, gz_recodata = np.asarray(gx_recodata), np.asarray(gy_recodata), np.asarray(gz_recodata)
-  g_recodata = np.abs(np.sqrt((gx_recodata**2)+(gy_recodata**2)+(gz_recodata**2)))
-  return g_recodata
-   
-
-
-
-
+    fig1 = plt.figure()
+    ax = fig1.add_subplot(projection='3d')
+    scalesz = np.max(recodata_all_ev[i].shape) * 12 / 1.6
+    ax.set_xlim([-scalesz, scalesz])
+    ax.set_ylim([-scalesz, scalesz])
+    ax.set_zlim([-scalesz, scalesz])
+    ax.scatter3D(centers_all_ev[i][:, 0], centers_all_ev[i][:,1], centers_all_ev[i][:,2], c = y_pred, cmap = 'cividis',s=15)
+    for c in range(len(all_curves_in_ev)):
+      ax.scatter3D(all_curves_in_ev[c][:, 0], all_curves_in_ev[c][:,1], all_curves_in_ev[c][:,2], color = 'red')
+    plt.title(events[i])
+    #plt.legend()
+  plt.show()
+  
 
 
 if __name__ == '__main__':
@@ -141,70 +188,14 @@ if __name__ == '__main__':
   geometryPath = "./geometry" #path to GRAIN geometry
   fpkl1 = "./data/initial-data/reco_data/3dreco-0-10.pkl" #reconstruction data file
   fpkl2 = "./data/initial-data/reco_data/3dreco-10-30.pkl"
-  #eventNumber = selectedEvents[0]
   geom = load_geometry(geometryPath, defs)
-  """normal pars"""
-  it = 500
-  cuts = [100, 500] #cuts on voxel amplitude: higher and lower
-  cut = cuts[1] 
-
-
-  centers_all_ev = []
-  amps_all_ev = []
-  for ev in selectedEvents:
-    data = load_pickle(fpkl1, ev)
-    if it not in data.keys():
-      data = load_pickle(fpkl2, ev)
-    recodata = data[it]
-    data_masked = recodata[5:-5,5:-5,5:-5]
-    centers, amps = getVoxelsCut(data_masked, geom.fiducial, cut)
-    centers = np.transpose(centers)
-    centers_all_ev.append(centers)
-    amps_all_ev.append(amps)
-
-  # """gradient pars"""
-  # g_it = 240
-  # g_cut = 50 #cut on voxel amplitude
-  # g_recodata = data[g_it]
-  # g_data_masked = g_recodata[5:-5,5:-5,5:-5]
-  # gradient_recodata = gradient(g_data_masked)
-  # g_centers, g_amps = getVoxelsCut(gradient_recodata, geom.fiducial, g_cut)
-  # g_centers = np.transpose(g_centers)
-
-  for i in range(len(centers_all_ev)):
-    all_clusters_in_ev, all_amps_in_ev, y_pred = Clustering(centers_all_ev[i], amps_all_ev[i])
-    # print(len(all_amps_in_ev))
-    all_curves_in_ev = ClustersLPC(all_clusters_in_ev,all_amps_in_ev)
-    
-
-    fig1 = plt.figure()
-    ax = fig1.add_subplot(projection='3d')
-    ax.scatter3D(centers_all_ev[i][:, 0], centers_all_ev[i][:,1], centers_all_ev[i][:,2], c = y_pred, cmap = 'cividis',s=15)
-    #DA SISTEMARE IL PLOT DELLE CURVE!!!!
-    for c in range(len(all_curves_in_ev)):
-      ax.scatter3D(all_curves_in_ev[c][:, 0], all_curves_in_ev[c][:,1], all_curves_in_ev[c][:,2], color = 'red')
-    plt.title(selectedEvents[i])
-    plt.legend()
-    plt.show()
   
-  # """normal"""
-  # fig1 = plt.figure()
-  # ax = fig1.add_subplot(projection='3d')
-  # # for i in range(len(all_curves)):
-  # #   ax.scatter3D(all_curves[i][:, 0], all_curves[i][:,1], all_curves[i][:,2], color = 'red')
-  # #   # ax.scatter3D(all_curves[i][0,0], all_curves[i][:,1], all_curves[i][:,2], color = 'green')
-  # ax.scatter3D(centers[:, 0], centers[:,1], centers[:,2], c = amps, cmap = 'viridis', alpha = 0.3)
-  # plt.legend()
-  # #plt.show()
+  #centers_all_ev, amps_all_ev, recodata_all_ev = AllCentersAllEvents(selectedEvents, fpkl1, fpkl2, applyGradient=False)
 
-  # """gradient"""
-  # fig2 = plt.figure()
-  # ax = fig2.add_subplot(projection='3d')
-  # # for i in range(len(all_curves)):
-  # #   ax.scatter3D(all_curves[i][:, 0], all_curves[i][:,1], all_curves[i][:,2], color = 'red')
-  # #   # ax.scatter3D(all_curves[i][0,0], all_curves[i][:,1], all_curves[i][:,2], color = 'green')
-  # ax.scatter3D(g_centers[:, 0], g_centers[:,1], g_centers[:,2], c = g_amps, cmap = 'cividis', alpha = 0.3)
-  # plt.legend()
-  # #plt.show()
+  #g_centers_all_ev, g_amps_all_ev, g_recodata_all_ev = AllCentersAllEvents(selectedEvents, fpkl1, fpkl2, applyGradient=True)
 
+  """only amps"""
+  #PlotClusters(selectedEvents, fpkl1, fpkl2, applyGradient=False)
 
+  """gradient amps"""
+  PlotClusters(selectedEvents, fpkl1, fpkl2, applyGradient=True)
