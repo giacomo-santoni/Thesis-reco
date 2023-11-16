703b496fde48d0b0c0b179c3d32e15fec400b47c
giacomo-santoni <133137485+giacomo-santoni@users.noreply.github.com>
2023-11-16 12:15:36 +0100

    new lpc attempt and pca

--- mask-reco-tools/lpc_analysis.py
+++ mask-reco-tools/lpc_analysis.py
@@ -6,16 +6,18 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import DBSCAN
 import seaborn as sns
 import kneed as kn
+import sys
 
 from lpcclass import LPCSolver
 from recodisplay import load_pickle, parseArgs
 from geom_import import load_geometry
+from recoprocessing import compute_pca
 
 def SetParameters():
    epsilon = 36
    min_points = 6
-   bandwidth = 30
-   stepsize = 30
+   bandwidth = 40
+   stepsize = 40
 
    cuts = [100,500]
    g_cut_perc = 0.05
@@ -76,7 +78,7 @@ def AllCentersAllEvents(events, fname1, fname2, applyGradient):
       data = load_pickle(fname2, ev)
     if applyGradient:
        recodata = data[g_it]
-       data_masked = recodata[7:-7,7:-7,7:-7]
+       data_masked = recodata[5:-5,5:-5,5:-5]
        gradient_recodata = gradient(data_masked)
        g_cut = g_cut_perc*np.max(gradient_recodata)
        centers, amps = getVoxelsCut(gradient_recodata, geom.fiducial, g_cut)
@@ -91,7 +93,6 @@ def AllCentersAllEvents(events, fname1, fname2, applyGradient):
     amps_all_ev.append(amps)
   recodata_all_ev = np.asarray(recodata_all_ev)
   return centers_all_ev, amps_all_ev, recodata_all_ev
-
 
 def Clustering(centers, amps):
     epsilon, min_points, _, _, _, _, _, _ = SetParameters()
@@ -132,46 +133,98 @@ def Clustering(centers, amps):
        all_amps_in_ev.append(amp)
     return all_clusters_in_ev, all_amps_in_ev, y_pred, labels
 
-def LPC(centers, amps):
+def LPC(centers, amps, startPoint):
     _,_,h,t,_,_,_,_ = SetParameters()
     """compute LPC"""
     lpc = LPCSolver()
     lpc.setPoints(centers, amps)
     lpc.setBandwidth(h) #find right parameter
     lpc.setStepSize(t) #find right parameter  
-    lpc.solve()      
+    lpc.solve(startPoint)      
     #lpc.plot()
     curve = lpc.lpcPoints
+    inversionPoint = lpc.inversionPoint
+    inversionPoint = np.asarray(inversionPoint)
+    endPoint = curve[len(curve)-1]
+    #initialPoint = np.average(centers, axis = 0, weights = amps)
+    #initialPoint = lpc.setMaxAmpPoint(centers,amps)
     #plt.show()  
-    return curve
+    return curve, inversionPoint, endPoint
 
-def ClustersLPC(clusters, amps, labels):
+def ClustersLPC(clusters, amps, labels, startPoint):
     all_curves = []
+    all_inv_points = []
+    all_end_points = []
+    #all_initial_points = []
     for i in range(len(clusters)):
-      #print(labels[i])
       if labels[i] != -1:
-        curve = LPC(clusters[i], amps[i])
+        curve, inversionPoint, endPoint = LPC(clusters[i], amps[i], startPoint[i])
         curve = np.asarray(curve)
         all_curves.append(curve)
-    return all_curves
+        all_inv_points.append(inversionPoint)
+        all_end_points.append(endPoint)
+        #all_initial_points.append(initialPoint)
+    all_inv_points = np.asarray(all_inv_points)
+    all_end_points = np.asarray(all_end_points)
+    #all_initial_points = np.asarray(all_initial_points)
+    return all_curves, all_inv_points, all_end_points#, all_initial_points
+
+# def NewLPC(clusters, amps, labels):
+  # all_curves, all_inv_points, all_end_points = ClustersLPC(clusters, amps, labels)
+  # for i in range(len(all_curves)): #IMPORTANTE!! nr di clusters è diverso dal numero di curves, dal momento nei clusters è contato anche il cluster del noise mentre nelle curves no
+  #   original_stdout = sys.stdout
+  #   with open('./points.txt', 'a') as f:
+  #       sys.stdout = f
+  #       print('cluster nr: ', i+1)
+  #       print('inversion point: ', all_inv_points[i])
+  #       print('end point: ', all_end_points[i])
+  #       sys.stdout = original_stdout
+  #   n = 0
+  #   print('cluster nr: ', i+1)
+  #   for points in clusters[i]:
+  #      if all_inv_points[i][0] <= points[0] <= all_end_points[i][0] and all_inv_points[i][1] <= points[1] <= all_end_points[i][1] and all_inv_points[i][2] <= points[2] <= all_end_points[i][2]:
+  #         n += 1
+  #         print(n)
+
+def NewClustersLPC(clusters, amps, startPoint):
+  all_curves_new = []
+  #all_initial_points = []
+  for i in range(len(clusters)-1):
+      curve, _, _ = LPC(clusters[i], amps[i], startPoint[i])
+      curve = np.asarray(curve)
+      all_curves_new.append(curve)
+  return all_curves_new
 
 def PlotClusters(events, fname1, fname2, applyGradient):
+  centers_all_ev, amps_all_ev, recodata_all_ev = AllCentersAllEvents(events, fname1, fname2, applyGradient)
   for i in range(len(events)):
-    centers_all_ev, amps_all_ev, recodata_all_ev = AllCentersAllEvents(events, fname1, fname2, applyGradient)
     all_clusters_in_ev, all_amps_in_ev, y_pred, labels= Clustering(centers_all_ev[i], amps_all_ev[i])
-    all_curves_in_ev = ClustersLPC(all_clusters_in_ev,all_amps_in_ev,labels)
-    
+    all_curves_in_ev, all_inv_points, all_end_points = ClustersLPC(all_clusters_in_ev,all_amps_in_ev,labels)
+    #original_stdout = sys.stdout
+    # with open('./points.txt', 'a') as f:
+    #     sys.stdout = f
+    #     print('event: ', events[i])
+    #     sys.stdout = original_stdout
+    # print('event: ', events[i])
+    # NewLPC(all_clusters_in_ev, all_amps_in_ev, labels)
+  
     fig1 = plt.figure()
     ax = fig1.add_subplot(projection='3d')
     scalesz = np.max(recodata_all_ev[i].shape) * 12 / 1.6
     ax.set_xlim([-scalesz, scalesz])
     ax.set_ylim([-scalesz, scalesz])
     ax.set_zlim([-scalesz, scalesz])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
     ax.scatter3D(centers_all_ev[i][:, 0], centers_all_ev[i][:,1], centers_all_ev[i][:,2], c = y_pred, cmap = 'cividis',s=15)
     for c in range(len(all_curves_in_ev)):
-      ax.scatter3D(all_curves_in_ev[c][:, 0], all_curves_in_ev[c][:,1], all_curves_in_ev[c][:,2], color = 'red')
+      #ax.scatter3D(all_curves_in_ev[c][:, 0], all_curves_in_ev[c][:,1], all_curves_in_ev[c][:,2], color = 'red')
+      ax.scatter3D(all_curves_in_ev[c][0,0], all_curves_in_ev[c][0,1], all_curves_in_ev[c][0,2], color = 'darkorange')
+      ax.scatter3D(all_inv_points[c][0],all_inv_points[c][1],all_inv_points[c][2], color = 'cyan')
+      ax.scatter3D(all_end_points[c][0],all_end_points[c][1],all_end_points[c][2], color = 'black')
     plt.title(events[i])
-    #plt.legend()
+    plt.legend()
   plt.show()
   
 
@@ -180,7 +233,7 @@ if __name__ == '__main__':
   ###example structure, fill the gaps
   
   eventNumbers = EventNumberList("./data/initial-data/EDepInGrain_1.txt")
-  #ev selected: 229,265,440,1173,1970,3344,3453,3701,4300
+  # ev selected: 229,265,440,1173,1970,3344,3453,3701,4300
   selectedEvents = [eventNumbers[0],eventNumbers[1],eventNumbers[2],eventNumbers[6],eventNumbers[9],eventNumbers[20],eventNumbers[21],eventNumbers[24],eventNumbers[25]]
 
   defs = {}
@@ -190,12 +243,54 @@ if __name__ == '__main__':
   fpkl2 = "./data/initial-data/reco_data/3dreco-10-30.pkl"
   geom = load_geometry(geometryPath, defs)
   
-  #centers_all_ev, amps_all_ev, recodata_all_ev = AllCentersAllEvents(selectedEvents, fpkl1, fpkl2, applyGradient=False)
+  centers_all_ev, amps_all_ev, recodata_all_ev = AllCentersAllEvents(selectedEvents, fpkl1, fpkl2, applyGradient=False)
+  #print(list(centers_all_ev[0]))
 
-  #g_centers_all_ev, g_amps_all_ev, g_recodata_all_ev = AllCentersAllEvents(selectedEvents, fpkl1, fpkl2, applyGradient=True)
+  data = load_pickle(fpkl1, 229)
+  recodata = data[500]
+  data_masked = recodata[5:-5,5:-5,5:-5]
+  pca_results = compute_pca(data_masked, geom.fiducial, 100, plot=True)
+  print(pca_results)
+ 
+
+  # # g_centers_all_ev, g_amps_all_ev, g_recodata_all_ev = AllCentersAllEvents(selectedEvents, fpkl1, fpkl2, applyGradient=True)
+  
+  # for i in range(len(selectedEvents)):
+  #   all_clusters_in_ev, all_amps_in_ev, y_pred, labels = Clustering(centers_all_ev[i], amps_all_ev[i])
+  #   all_initial_points = []
+  #   for c in range(len(all_clusters_in_ev)):
+  #     initialPoint = np.average(all_clusters_in_ev[c], axis = 0, weights = all_amps_in_ev[c])
+  #     all_initial_points.append(initialPoint)
+  #   all_curves_in_ev, all_inv_points, all_end_points = ClustersLPC(all_clusters_in_ev, all_amps_in_ev, labels, all_initial_points)
+    
+  #   # all_curves_in_ev2 = NewClustersLPC(all_clusters_in_ev, all_amps_in_ev, all_inv_points)
+
+  #   fig1 = plt.figure()
+  #   ax = fig1.add_subplot(projection='3d')
+  #   scalesz = np.max(recodata_all_ev[i].shape) * 12 / 1.6
+  #   ax.set_xlim([-scalesz, scalesz])
+  #   ax.set_ylim([-scalesz, scalesz])
+  #   ax.set_zlim([-scalesz, scalesz])
+  #   ax.set_xlabel('x')
+  #   ax.set_ylabel('y')
+  #   ax.set_zlabel('z')
+  #   ax.scatter3D(centers_all_ev[i][:, 0], centers_all_ev[i][:,1], centers_all_ev[i][:,2], c = y_pred, cmap = 'cividis',s=15)
+  #   for c in range(len(all_curves_in_ev)):
+  #     ax.scatter3D(all_curves_in_ev[c][:, 0], all_curves_in_ev[c][:,1], all_curves_in_ev[c][:,2], color = 'red')
+  #     # ax.scatter3D(all_curves_in_ev2[c][:, 0], all_curves_in_ev2[c][:,1], all_curves_in_ev2[c][:,2], color = 'darkgoldenrod')
+  #     # ax.scatter3D(all_curves_in_ev[c][0,0], all_curves_in_ev[c][0,1], all_curves_in_ev[c][0,2], color = 'darkorange')
+  #     # ax.scatter3D(all_inv_points[c][0],all_inv_points[c][1],all_inv_points[c][2], color = 'cyan')
+  #     # ax.scatter3D(all_curves_in_ev[c][len(all_curves_in_ev[c])-1,0], all_curves_in_ev[c][len(all_curves_in_ev[c])-1,1], all_curves_in_ev[c][len(all_curves_in_ev[c])-1,2], color = 'black')
+  #   plt.title(selectedEvents[i])
+  #   plt.legend()
+  #   plt.show()
+  
+
+
+
 
   """only amps"""
-  #PlotClusters(selectedEvents, fpkl1, fpkl2, applyGradient=False)
+  # PlotClusters(selectedEvents, fpkl1, fpkl2, applyGradient=False)
 
   """gradient amps"""
-  PlotClusters(selectedEvents, fpkl1, fpkl2, applyGradient=True)
+  #PlotClusters(selectedEvents, fpkl1, fpkl2, applyGradient=True)
